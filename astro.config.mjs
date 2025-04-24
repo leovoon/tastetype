55364f599e5daf4bd572a4b1e56d1a50a83d5135
leovoon <voonlihhaur@gmail.com>
2025-04-24 23:18:48 +0800

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()]
 	},
+	output: 'server',
+	adapter: vercel(),
 	env: {
 		schema: {
 			GEMINI_API_KEY: envField.string({ context: "server", access: "secret" }),
